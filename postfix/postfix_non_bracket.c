9da1c007e355dd14c6db87390730b0b35daf1199
FrenchRuin <toxic023@naver.com>
2022-08-24 11:04:46 +0900

    postfix

--- postfix/postfix_non_bracket.c
+++ postfix/postfix_non_bracket.c
@@ -35,7 +35,7 @@ char *convert(char *infix)
         }
         token = strtok(NULL, " ");
     }
-    while (!is_empty(operator_stack)) {
+    while (!is_empty(operator_stack)) {  //while not empty operator_stack
         char op = (char) pop(operator_stack); //
         sprintf(pos, "%c", op);
         pos += 2;
@@ -66,23 +66,23 @@ void handle_exception(const char *err_msg){
 
 char *process_op(char op, char *pos)
 {
-    if (is_empty(operator_stack)) {
-        push(operator_stack, op);
-    }else{
+    if (is_empty(operator_stack)) {   // if operator_stack is empty
+        push(operator_stack, op); // then push operator push on operator_stack
+    }else{                                            // if operator_stack is not empty
         char top_op = peek(operator_stack);
-        if (precedence(op) > precedence(top_op)) {
-            push(operator_stack, op);
+        if (precedence(op) > precedence(top_op)) { // if operator_stack top stack is lower than parameter op
+            push(operator_stack, op);           // then push parameter op push on operator_stack
         }else{
-            while (!is_empty(operator_stack) && precedence(op) <= precedence(top_op)) {
-                pop(operator_stack);
-                sprintf(pos, "%c", top_op);
-                pos += 2;
-                if (!is_empty(operator_stack)) {
+            while (!is_empty(operator_stack) && precedence(op) <= precedence(top_op)) { // while operator stack not empty ...
+                pop(operator_stack);               // pop
+                sprintf(pos, "%c", top_op);  // top_op append on pos
+                pos += 2;     // blank space
+                if (!is_empty(operator_stack)) {           // doing 'while' statement, operator_stack is not empty top_op is peek.
                     top_op = (char) peek(operator_stack);
                 }
-                push(operator_stack, op);
+                push(operator_stack, op); // op push on operator_stack.
             }
         }
     }
-    return pos;
+    return pos;  // then
 }
